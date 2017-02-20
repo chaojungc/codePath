0fe90cbf9e4afd86d2e4619d9955795aef31cd90
ChaoJung <chaojung@outlook.com>
2017-02-20 23:48:53 +0800

    add actionBar and recyclerView function

---
+++ Week2Assignment/app/src/main/java/com/example/chaojung/nytimessearch/Article.java
@@ -0,0 +1,16 @@
+package com.example.chaojung.nytimessearch;
+
+/**
+ * Created by ChaoJung on 2017/2/20.
+ */
+
+public class Article {
+
+    String webUrl;
+    String headline;
+    String thumbNail;
+
+
+
+
+}
--- Week2Assignment/app/src/main/java/com/example/chaojung/nytimessearch/SearchActivity.java
+++ Week2Assignment/app/src/main/java/com/example/chaojung/nytimessearch/SearchActivity.java
@@ -1,52 +1,66 @@
 package com.example.chaojung.nytimessearch;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+
+import java.util.ArrayList;
 
 public class SearchActivity extends AppCompatActivity {
 
+    ArrayList<Article> articles;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        /*
+        // Lookup the recyclerview in activity layout
+        RecyclerView rvArticles = (RecyclerView) findViewById(R.id.rvArticles);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
+        // Initialize contacts
+        articles = Article.createContactsList(20);
+        // Create adapter passing in the sample user data
+        ArticlesAdapter adapter = new ArticlesAdapter(this, articles);
+        // Attach the adapter to the recyclerview to populate items
+        rvArticles.setAdapter(adapter);
+        // Set layout manager to position the items
+        rvArticles.setLayoutManager(new LinearLayoutManager(this));
+        // That's all!
+        */
+
+    } //API Key = b04b71a746fa4bc5aaf6f146e9b7cff0
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_search, menu);
-        return true;
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_search, menu);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // perform query here
+
+                // workaround to avoid issues with some emulators and keyboard devices firing twice if a keyboard enter is used
+                // see https://code.google.com/p/android/issues/detail?id=24599
+                searchView.clearFocus();
+
+                return true;
+            }
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
